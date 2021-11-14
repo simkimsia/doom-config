72bc6721ae070161160af6456619a7859c4fcc45
Jethro Kuan <jethro.kuan@bytedance.com>
2021-11-14 09:59:36 +0800

    add org-agenda/capture config

--- config.el
+++ config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Iosevka SS04" :size 24 :weight 'light)
-      doom-variable-pitch-font (font-spec :family "SF Pro Text" :size 20))
+(setq doom-font (font-spec :family "Iosevka" :size 24 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "Roboto" :size 20))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -29,7 +29,7 @@
 
 ;;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Dendron/vault/org/")
+(setq org-directory "~/kimsia-org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -56,8 +56,33 @@
 
 ;; for some reason the projectile discovery-projects-in-discovery is now missing
 ;; so i am following this iadvice https://www.reddit.com/r/DoomEmacs/comments/qiwfj2/comment/him9olr/?utm_source=share&utm_medium=web2x&context=3
-(setq projectile-project-search-path '("~/Dendron/vault/org/" "~/Projects/"))
+;; (setq projectile-project-search-path '("~/Dendron/vault/org/" "~/Projects/"))
 
 ;; M-x projectile-discover-projects-in-search-path
 ;;(add-hook 'after-init-hook #'toggle-frame-maximized)
 (delete-selection-mode 1)
+
+(after! org
+  (setq org-capture-templates
+        `(("i" "Inbox" entry (file "todo.org")
+           ,(concat "* TODO %?\n"
+                    "/Entered on/ %u")))))
+
+(defun kimsia/org-inbox-capture ()
+  (interactive)
+  "Capture a task in agenda mode."
+  (org-capture nil "i"))
+
+(map! :map org-agenda-mode-map "c" #'kimsia/org-inbox-capture)
+
+(setq org-agenda-custom-commands `((" " "Agenda"
+                                      ((agenda ""
+                                               ((org-agenda-span 'week)
+                                                (org-deadline-warning-days 365)))
+                                       (todo "TODO"
+                                             ((org-agenda-overriding-header "Inbox")
+                                              (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled 'deadline))))
+                                       (tags "proj1"
+                                             ((org-agenda-overriding-header "Project 1")))
+                                       (tags "proj2"
+                                             ((org-agenda-overriding-header "Project 2")))))))
